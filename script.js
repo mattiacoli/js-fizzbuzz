e193bc907f9e34c98365efb9619040ee9a779da4
mattiacoli <mattia.coli@aol.com>
2025-02-11 15:23:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e 
+// per i multipli di 5 stampi Buzz. 
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// Preparazione
+
+// stampare numeri da 1 a 100 con loop for
+
